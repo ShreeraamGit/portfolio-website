99ee93562c8d9739a47c3abcaa43e680b832106b
Shreeraam Alagarsamy Sethuraj <shreeraaam@outlook.com>
2022-01-29 09:53:09 +0100

    structure of html created

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-bg': '#F7FFF7',
+        'content': '#1A535C',
+        'heading':'#FF6B6B',
+        'aux':'#4ECDC4'
+      },
+      fontFamily: {
+        'main-font': ['Sora']
+      }
+    },
   },
   plugins: [],
 }
