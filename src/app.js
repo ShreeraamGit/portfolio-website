3b54b8512bfcf7b6a39838f21893d705ea0c48f1
Shreeraam Alagarsamy Sethuraj <shreeraaam@outlook.com>
2022-01-30 19:34:27 +0100

    menu open close implemented

--- src/app.js
+++ src/app.js
@@ -1,3 +1,17 @@
-const intViewportHeight = window.innerHeight;
+const hamburgerOpen = document.querySelector('.hamburger__open')
+const hamburgerClose = document.querySelector('.hamburger__close')
+const navLinks = document.querySelector('.nav__links')
+console.log(navLinks)
+hamburgerOpen.addEventListener('click',function(e){
+    console.log('open')
+    hamburgerClose.classList.remove('invisible');
+    hamburgerOpen.classList.remove('visible');
+    hamburgerOpen.classList.add('invisible');
+})
 
-console.log(intViewportHeight)
+hamburgerClose.addEventListener('click',function(e){
+    console.log('close')
+    hamburgerClose.classList.add('invisible')
+    hamburgerOpen.classList.remove('invisible');
+    hamburgerOpen.classList.add('visible');
+})
