5535c7fd91fa354caa70d37dfbbe93293894ada4
Shreeraam Alagarsamy Sethuraj <shreeraaam@outlook.com>
2022-02-05 12:41:19 +0100

    added prettier

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
 //// Hamburger menu
 
-const hamburgerOpen = document.querySelector('.hamburger__open')
-const hamburgerClose = document.querySelector('.hamburger__close')
-const navLinks = document.querySelector('.nav__links')
-console.log(navLinks)
-hamburgerOpen.addEventListener('click',function(e){
-    console.log('open')
-    hamburgerClose.classList.remove('invisible');
-    hamburgerOpen.classList.remove('visible');
-    hamburgerOpen.classList.add('invisible');
-})
+const hamburgerOpen = document.querySelector(".hamburger__open");
+const hamburgerClose = document.querySelector(".hamburger__close");
+const navLinks = document.querySelector(".nav__links");
+////console.log(navLinks);
+hamburgerOpen.addEventListener("click", function (e) {
+  ////console.log("open");
+  hamburgerClose.classList.remove("invisible");
+  hamburgerOpen.classList.remove("visible");
+  hamburgerOpen.classList.add("invisible");
+});
 
-hamburgerClose.addEventListener('click',function(e){
-    console.log('close')
-    hamburgerClose.classList.add('invisible')
-    hamburgerOpen.classList.remove('invisible');
-    hamburgerOpen.classList.add('visible');
-})
+hamburgerClose.addEventListener("click", function (e) {
+  ////console.log("close");
+  hamburgerClose.classList.add("invisible");
+  hamburgerOpen.classList.remove("invisible");
+  hamburgerOpen.classList.add("visible");
+});
