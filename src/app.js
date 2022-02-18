36a136f15adcf28b56365c2e6fcd72bfc8cb09c7
Shreeraam Alagarsamy Sethuraj <shreeraaam@outlook.com>
2022-02-18 06:57:32 +0100

    working on slide in contents

--- src/app.js
+++ src/app.js
@@ -219,7 +219,7 @@ const revealSection = function (entries, observer) {
 };
 const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
-  threshold: 0.15,
+  threshold: 0.5,
 });
 sections.forEach(function (section) {
   sectionObserver.observe(section);
