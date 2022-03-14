388e89a9185ab2367c96cb9590c02710b8165292
Shreeraam Alagarsamy Sethuraj <shreeraaam@outlook.com>
2022-03-14 06:53:15 +0100

    final draft 5

--- src/app.js
+++ src/app.js
@@ -220,7 +220,7 @@ setInterval(function () {
 }, 10000);
 
 /*-------------------------------------------------------------*/
-/*
+
 /// Reveal Sections animations
 const sections = document.querySelectorAll(".section");
 const revealSection = function (entries, observer) {
@@ -239,7 +239,7 @@ sections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add("section--hidden");
 });
-*/
+
 /*-------------------------------------------------------------*/
 
 ///Dark mode enable/disable -- mobile mode
