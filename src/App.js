9a9656c2510805d6ca88a063084d7761b7df3fca
Shreeraam Alagarsamy Sethuraj <shreeraaam@outlook.com>
2022-12-11 23:47:05 +0100

    tried code splitting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import { Routes, Route } from 'react-router-dom';
-import { Home, Projects, Blogs, MainPage } from './routes';
+import { Home, MainPage } from './routes';
+import React, { Suspense, lazy } from 'react';
+
+const Blogs = lazy(() => import('./routes/Blogs'));
+const Projects = lazy(() => import('./routes/Projects'));
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<MainPage></MainPage>}>
-          <Route index element={<Home />} />
-          <Route path="projects" element={<Projects />}>
-            <Route
-              path=":projectsId"
-              element={<div className="">Inside Projects Page</div>}
-            />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainPage></MainPage>}>
+            <Route index element={<Home />} />
+            <Route path="projects" element={<Projects />}>
+              <Route
+                path=":projectsId"
+                element={<div className="">Inside Projects Page</div>}
+              />
+            </Route>
+            <Route path="blogs" element={<Blogs />}>
+              <Route
+                path=":blogsId"
+                element={<div className="">Inside Projects Page</div>}
+              />
+            </Route>
           </Route>
-          <Route path="blogs" element={<Blogs />}>
-            <Route
-              path=":blogsId"
-              element={<div className="">Inside Projects Page</div>}
-            />
-          </Route>
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </>
   );
 }
