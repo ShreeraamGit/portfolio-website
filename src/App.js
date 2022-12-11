087e26e0dcea4576f9f296c5e78f3bb5b316294c
Shreeraam Alagarsamy Sethuraj <shreeraaam@outlook.com>
2022-12-12 00:03:11 +0100

    lazy loading experiement

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
 import { Routes, Route } from 'react-router-dom';
-import { Home, MainPage } from './routes';
+import { MainPage } from './routes';
 import React, { Suspense, lazy } from 'react';
 
+const Home = lazy(() => import('./routes/Home'));
 const Blogs = lazy(() => import('./routes/Blogs'));
 const Projects = lazy(() => import('./routes/Projects'));
 
 function App() {
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<MainPage></MainPage>}>
-            <Route index element={<Home />} />
-            <Route path="projects" element={<Projects />}>
-              <Route
-                path=":projectsId"
-                element={<div className="">Inside Projects Page</div>}
-              />
-            </Route>
-            <Route path="blogs" element={<Blogs />}>
-              <Route
-                path=":blogsId"
-                element={<div className="">Inside Projects Page</div>}
-              />
-            </Route>
+      <Routes>
+        <Route path="/" element={<MainPage></MainPage>}>
+          <Route index element={<Home />} />
+          <Route path="projects" element={<Projects />}>
+            <Route
+              path=":projectsId"
+              element={<div className="">Inside Projects Page</div>}
+            />
           </Route>
-        </Routes>
-      </Suspense>
+          <Route path="blogs" element={<Blogs />}>
+            <Route
+              path=":blogsId"
+              element={<div className="">Inside Projects Page</div>}
+            />
+          </Route>
+        </Route>
+      </Routes>
     </>
   );
 }
--- src/routes/MainPage.jsx
+++ src/routes/MainPage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Navbar } from '../components';
 import { Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
 
 const MainPage = () => {
   return (
     <main className="bg-primary-black overflow-hidden">
       <Navbar />
-      <Outlet />
+      <Suspense fallback={<div className="">Loading .....</div>}>
+        <Outlet />
+      </Suspense>
     </main>
   );
 };
