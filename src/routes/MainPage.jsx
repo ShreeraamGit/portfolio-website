4c07a162a36a17b5e04ee77a386828000d0dab02
Shreeraam Alagarsamy Sethuraj <shreeraaam@outlook.com>
2022-12-12 10:35:10 +0100

    removed loading effect

--- src/routes/MainPage.jsx
+++ src/routes/MainPage.jsx
@@ -5,18 +5,8 @@ import { Suspense } from 'react';
 import { motion, useScroll, useSpring } from 'framer-motion';
 
 const MainPage = () => {
-  const { scrollYProgress } = useScroll();
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 100,
-    damping: 30,
-    restDelta: 0.001,
-  });
   return (
     <main className="bg-primary-black overflow-hidden">
-      <motion.div
-        style={{ scaleX }}
-        className="fixed top-0 left-0 right-0 h-[10px] bg-cyan-600 origin-[0%]"
-      />
       <Navbar />
       <Suspense fallback={<div className="">Loading .....</div>}>
         <Outlet />
