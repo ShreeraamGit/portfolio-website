565bae2f4263a8d3698dafc33fef4eb6e5a084c6
Shreeraam Alagarsamy Sethuraj <shreeraaam@outlook.com>
2022-12-12 10:30:27 +0100

    loading page screen implemented

--- src/components/SkillsIconAnimation.jsx
+++ src/components/SkillsIconAnimation.jsx
@@ -37,18 +37,3 @@ const SkillsIconAnimation = () => {
 };
 
 export default SkillsIconAnimation;
-
-/*<motion.div
-      variants={slideIn('left', 'tween', 1.5, 1)}
-      className="relative w-full md:-mt-[20px] -mt-[12px]"
-    >
-      <div className={`flex justify-center items-center ${styles.paddings} `}>
-        <div className="flex w-full lg:w-[40%] justify-evenly">
-          <Icon as={SiJavascript} boxSize={50} color="#F6E05E" />
-          <Icon as={FaReact} boxSize={50} color="#76E4F7" />
-          <Icon as={SiRedux} boxSize={50} color="#6B46C1" />
-          <Icon as={SiNextdotjs} boxSize={50} color="#FFFFFF" />
-          <Icon as={DiNpm} boxSize={50} color="#E53E3E" />
-        </div>
-      </div>
-    </motion.div>*/
--- src/routes/MainPage.jsx
+++ src/routes/MainPage.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import { Navbar } from '../components';
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
+import { motion, useScroll, useSpring } from 'framer-motion';
 
 const MainPage = () => {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
   return (
     <main className="bg-primary-black overflow-hidden">
+      <motion.div
+        style={{ scaleX }}
+        className="fixed top-0 left-0 right-0 h-[10px] bg-red-600 origin-[0%]"
+      />
       <Navbar />
       <Suspense fallback={<div className="">Loading .....</div>}>
         <Outlet />
